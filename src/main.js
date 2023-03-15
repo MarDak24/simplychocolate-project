6c8bccdb562264e31fdf4defa0097c3eb53871ef
MarDak24 <maxim.matjerow@gmail.com>
2023-03-16 00:19:23 +0200

    Start setting

---
+++ src/main.js
@@ -0,0 +1,16 @@
+Header
+Hero
+Advantages
+Unique components
+How it’s made?
+Our products
+Top sellers
+Reviews
+Subscribe
+Footer
+
+Buy now
+Thank you
+Leave a review about our chocolate
+Thank you for your feedback
+Explore our new chocolate first
